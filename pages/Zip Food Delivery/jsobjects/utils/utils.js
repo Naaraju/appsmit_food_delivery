4c7ba8373e169ccca816986f235a3374bc3e76a5
Naga Raju <nagu.gpatnam@gmail.com>
2023-04-19 03:41:21 +0000

    System generated commit, initial commit

---
+++ pages/Zip Food Delivery/jsobjects/utils/utils.js
@@ -0,0 +1,34 @@
+export default {
+	orderStatusOptions: [
+		
+  {
+    "label": "Order Placed",
+    "value": "ORDER PLACED"
+  },
+  {
+    "label": "Delayed",
+    "value": "DELAYED"
+  },
+  {
+    "label": "Delivered",
+    "value": "DELIVERED"
+  },
+  {
+    "label": "Refunded",
+    "value": "REFUNDED"
+  }
+],
+
+	addOrder: async () => {
+		const orderId = this.generateRandomID();
+		await add_order.run({id : orderId});
+		await add_product.run({id : orderId});
+		await fetch_orders.run();
+		await resetWidget('add_modal');
+		return closeModal('add_modal');
+	},
+	generateRandomID: () =>{
+		return '_' + Math.random().toString(36).substr(2,9)
+		
+	}
+}
